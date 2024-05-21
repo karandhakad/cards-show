4164174682177f90d58614124005bafbdebfc574
Karan Dhakad <karandhakad883@gmail.com>
2024-05-21 16:36:24 +0530

    change directory

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createContext } from 'react';
-import { liveData } from './Api';
+import { liveData } from './store/Api';
 
 const userContext = createContext();
 
